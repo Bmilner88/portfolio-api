7bab9bb8f9b4f01235276a6849d81d069f24ec7e
Ben Milner <bmilner88@gmail.com>
2022-08-28 17:33:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
+/* if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 };
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
+ */
 let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
